2893489947acca9897b4a051fdd8cd93f520a92a
Expediri <Warrior.USSR@yandex.ru>
2024-03-13 18:19:26 +0300

    Added a description to README

---
+++ README.txt
@@ -0,0 +1,3 @@
+This program forms a new array of strings from the existing array of strings, 
+the length of which is less than or equal to 3 characters.
+You can only change the contents and length of the array myArr
--- obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7548cad385bee7a9f639cc978864fd106ea58d64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
