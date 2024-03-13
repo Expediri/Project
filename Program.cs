b3a25a4c99439f150e6fb664e15e07ca8b67d4ab
Expediri <Warrior.USSR@yandex.ru>
2024-03-13 18:08:22 +0300

    Finished method Main and create method printArray

--- Program.cs
+++ Program.cs
@@ -4,6 +4,22 @@ class Finder
 {
     static void Main()
     {
-        
+        String [] myArr = {"Hello", "2", "world", ":-)"};
+        printArray (myArr);
+        Console.WriteLine("");
+        printArray (createArrayWithLengthOfThreeChar(myArr));
     }
+    static void printArray (String[] array)
+    {
+        Console.Write("[");
+        for (int i = 0;i < array.Length;i ++) {
+            Console.Write(array[i]);
+            if (i < array.Length - 1) 
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.Write("]");
+    }
+
 }
